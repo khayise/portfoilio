b94003d96f9075acea5f72053761b69e3d6b0102
khayise <dedesh.deu@gmail.com>
2024-10-14 21:46:25 +0200

    new fixes

--- src/Components/SideHeader.tsx
+++ src/Components/SideHeader.tsx
@@ -8,7 +8,7 @@ export const SideHeader: React.FC<Props> = ({}) => {
 	return (
 		<>
 			<header
-				className={`h-screen static w-full md:w-1/2 md:sticky pb-24 pt-24 left-0 top-0 bg-back flex flex-col justify-between items-start`}>
+				className={`h-screen static w-full pb-4 md:w-1/2 md:sticky  md:pb-12 pt-24 left-0 top-0 bg-back flex flex-col justify-between items-start`}>
 				<div className="flex flex-col">
 					<span className="text-accentColor font-main text-5xl font-semibold">Andrii Dedesh</span>
 					<span className="text-accentColor font-main text-xl font-light mt-3 mb-20">
