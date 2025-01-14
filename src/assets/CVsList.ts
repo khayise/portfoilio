41cde03962602d3f5fdfa4d6cc1d31cd4caaadee
khayise <dedesh.deu@gmail.com>
2025-01-14 19:33:50 +0100

    added a new cv

--- src/assets/CVsList.ts
+++ src/assets/CVsList.ts
@@ -7,22 +7,22 @@ export interface ICV {
 export const CVsList: ICV[] = [
 	{
 		language: 'EN',
-		href: '',
-		disabled: true,
+		href: 'https://www.dropbox.com/scl/fi/ddfcy7a5m5erid4qoxdxw/Frontend-developer-Andrii-Dedesh.pdf?rlkey=noimqjjq0ta8gmniscgt80gdr&st=kpy00kge&dl=1',
+		disabled: false,
 	},
 	{
 		language: 'DE',
-		href: '',
+		href: 'https://www.dropbox.com/scl/fi/0063whc6ihvvtfuttryjm/Frontend-entwickler-Andrii-Dedesh.pdf?rlkey=atwmloy8x14bjkq9ltwavoknz&st=7g6yv621&dl=1',
 		disabled: true,
 	},
 	{
 		language: 'RUS',
-		href: 'https://www.dropbox.com/scl/fi/dcuce2j3hpzmzfnchrvry/Andrii-Dedesh-CV-RUS.pdf?rlkey=n77u6msko3vaa233yqquvkqzw&st=tdvf1mdm&dl=1',
+		href: 'https://www.dropbox.com/scl/fi/kg4vgviz4puglongrxs60/Frontend-developer-Dedesh-Andrey.pdf?rlkey=cfjt7teasea0mg8osl6sy4d50&st=3rttf53n&dl=1',
 		disabled: false,
 	},
 	{
 		language: 'UKR',
-		href: 'https://www.dropbox.com/scl/fi/k1vi4wtf7o8g813tbl5xt/Andrii-Dedesh-CV-UKR.pdf?rlkey=v63loeqklzofo1q55kax6cm0m&st=i2twhvvt&dl=1',
+		href: 'https://www.dropbox.com/scl/fi/8ton1qzla550mheshdrmx/Frontend-developer-Dedesh-Andrii.pdf?rlkey=f3pxnv18yyjkx3eyk84ifxa04&st=pg8quxh4&dl=1',
 		disabled: false,
 	},
 ];
